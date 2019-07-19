f490603546670cafdaea6cb3c0c69d4511ceca81
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2019-07-19 12:59:56 +0530

    adding prompt from react-route to login page and link to login page also

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Fire from "../firebase/Fire";
+import { Prompt, Link } from "react-router-dom";
 
 export default class Login extends Component {
   constructor(props) {
@@ -59,6 +60,7 @@ export default class Login extends Component {
     return (
       <>
         <h1>Login</h1>
+        <Prompt when={true} message="are you sure you want to leave login page"></Prompt>
         <p>User name : {this.state.userUid}</p>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="email">Email</label>
@@ -78,6 +80,7 @@ export default class Login extends Component {
           <input type="submit" value={"Login"} />
           <input type="button" value={"Logout"} onClick={this.signOut} />
           <p>User email: {this.state.user}</p>
+          <Link to="/">Home</Link>
         </form>
       </>
     );
